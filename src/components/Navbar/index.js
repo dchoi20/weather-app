b5a2e39d19aff3b0b47c1957665ed768be376053
Danny Choi <choi.danny20@gmail.com>
2021-06-09 14:06:11 -0500

    added navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function Navbar () {
+    return (
+        <div class="topnav" id="myTopnav">
+            <a href="#home" class="active">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    )
+}
